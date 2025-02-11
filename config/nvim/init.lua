835afd6e93190f4fbf502f57633d4c3cf79c5f81
Martynas Kadiša <8273932+martynaskadisa@users.noreply.github.com>
2025-02-11 21:23:40 +0200

    update plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -555,7 +555,7 @@ require("lazy").setup({
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
-				-- rust_analyzer = {},
+				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
@@ -563,7 +563,7 @@ require("lazy").setup({
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				ts_ls = {},
-				--
+				eslint = {},
 
 				lua_ls = {
 					-- cmd = { ... },
